4c7de0a5cc36dc3b847bfe33db50d6fd36474ca2
Dujari, Sonali <sonali.dujari@hp.com>
2014-11-17 11:46:31 -0800

    Updates to template and colors

--- hp/js/pq-backgrounds.js
+++ hp/js/pq-backgrounds.js
@@ -1,15 +1,10 @@
 document.write("<div class='backgrounds hidden-xs'>\
 Background Color<br />\
-<div onclick=\"$('body').css('background-color','#FFFFFF');\" style='background-color: #FFFFFF;'></div>\
-<div onclick=\"$('body').css('background-color','#F7F7F7');\" style='background-color: #F7F7F7;'></div>\
-<div onclick=\"$('body').css('background-color','#F2F8F8');\" style='background-color: #F2F8F8;'></div>\
-<div onclick=\"$('body').css('background-color','#F5F6F7');\" style='background-color: #F5F6F7;'></div>\
-<div onclick=\"$('body').css('background-color','#FDF9F5');\" style='background-color: #FDF9F5;'></div>\
-<div onclick=\"$('body').css('background-color','#F3F7F5');\" style='background-color: #F3F7F5;'></div>\
-\
+<div onclick=\"$('body').css('background-color','#FFFFFF');\" style='background-color: #FFFFFF;'>White</div>\
+<div onclick=\"$('body').css('background-color','#F7F7F7');\" style='background-color: #F7F7F7;'>Grey</div>\
 </div>");
 
-$(document).ready(function() { 
+$(document).ready(function() {
 
 	$(".backgrounds").click(function() {
 		if ( $(this).css("right") == "-75px") {
